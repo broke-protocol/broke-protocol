9c18839110f64e68103cfaa91d04ae8317130149
NongBenz <nongbenzgames@gmail.com>
2019-12-07 12:45:05 +0700

    Added some sample text code for ExecuteCS and ExecuteJS

--- src/BP-GameSource/Types/CustomEvents.cs
+++ src/BP-GameSource/Types/CustomEvents.cs
@@ -17,6 +17,13 @@ namespace BrokeProtocol.CustomEvents
             if (player.job.info.groupIndex != GroupIndex.LawEnforcement)
             {
                 player.svPlayer.SendGameMessage($"Warning! You are about to enter {triggerID}!");
+
+                /* Execute client C# example */
+                //player.svPlayer.ExecuteCS("clManager.SendToServer(Channel.Unsequenced, SvPacket.GlobalMessage, \"ExecuteCS Test\");");
+
+                /* Execute client C# via JavaScript example */
+                /* Note inner quote is escaped twice due to being unwrapped across 2 languages */
+                //player.svPlayer.ExecuteJS("exec(\"clManager.SendToServer(Channel.Unsequenced, SvPacket.GlobalMessage, \\\"ExecuteJS Test\\\");\");");
             }
         }
     }
