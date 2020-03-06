d0fe6ff7847ead4bd51336e94d35e34d25657a1d
NongBenz <nongbenzgames@gmail.com>
2020-03-06 13:20:12 +0700

    EventsHandler will auto-register [CustomTarget] methods

--- BP-GameSource/CustomEvents.cs
+++ BP-GameSource/CustomEvents.cs
@@ -1,34 +1,12 @@
 ﻿using BrokeProtocol.API;
 using BrokeProtocol.Entities;
 using BrokeProtocol.Utility.Jobs;
-using System;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using UnityEngine;
 
 namespace BrokeProtocol.CustomEvents
 {
     public class CustomEvents : IScript
     {
-        public CustomEvents()
-        {
-            // Any public instance methods in this class will be auto registered
-            // Can call via EventHandler.Exec("Your Method Name") or EventHandler.Get("Your Method Name") if you need the return value
-            var methods = GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
-
-            foreach (var method in methods)
-            {
-                var types = method.GetParameters().Select(p => p.ParameterType);
-
-                Debug.Log($"[GS] Registering {method.Name} event");
-                if (!EventsHandler.Add(Delegate.CreateDelegate(Expression.GetActionType(types.ToArray()), this, method.Name)))
-                {
-                    Debug.LogWarning($"[GS] Event {method.Name} added more than once");
-                }
-            }
-        }
-
+        [CustomTarget]
         public void AreaWarning(ShPlayer player, string triggerID)
         {
             if (player.job.info.groupIndex != GroupIndex.LawEnforcement)
