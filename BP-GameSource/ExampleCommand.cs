415f7517c2bcddb83a582455eb7fa666f001deff
NongBenz <nongbenzgames@gmail.com>
2021-04-07 22:31:19 +0700

    Added SvOpenURL example command

--- BP-GameSource/ExampleCommand.cs
+++ BP-GameSource/ExampleCommand.cs
@@ -8,7 +8,7 @@ namespace BrokeProtocol.CustomEvents
     {
         public ExampleCommand()
         {
-            CommandHandler.RegisterCommand("Example", new Action<ShPlayer, ShPlayer, byte, int, float, string, string>(OnCommandInvoke), (player, command) =>
+            CommandHandler.RegisterCommand("Example", new Action<ShPlayer, ShPlayer, byte, int, float, string, string>(Command1), (player, command) =>
             {
                 // Silly example
                 if (player.health < 50)
@@ -19,31 +19,37 @@ namespace BrokeProtocol.CustomEvents
                 return true;
             });
 
-            CommandHandler.RegisterCommand("ExampleInt", new Action<ShPlayer, int>(OnCommandInvoke), null, "example.int");
-            CommandHandler.RegisterCommand("ExampleString", new Action<ShPlayer, string>(OnCommandInvoke), null, "example.string");
-            CommandHandler.RegisterCommand("ExamplePlayer", new Action<ShPlayer, ShPlayer>(OnCommandInvoke), null, "example.player");
+            CommandHandler.RegisterCommand("ExampleInt", new Action<ShPlayer, int>(Command2), null, "example.int");
+            CommandHandler.RegisterCommand("ExampleString", new Action<ShPlayer, string>(Command3), null, "example.string");
+            CommandHandler.RegisterCommand("ExamplePlayer", new Action<ShPlayer, ShPlayer>(Command4), null, "example.player");
+            CommandHandler.RegisterCommand("ExampleDiscord", new Action<ShPlayer>(Command5), null, "example.discord");
         }
 
 
         // Any optional parameters here will be optional with in-game commands too
-        public void OnCommandInvoke(ShPlayer player, ShPlayer target, byte byte1 = 1, int int1 = 2, float float1 = 3f, string string1 = "default1", string string2 = "default2")
+        public void Command1(ShPlayer player, ShPlayer target, byte byte1 = 1, int int1 = 2, float float1 = 3f, string string1 = "default1", string string2 = "default2")
         {
             player.svPlayer.SendGameMessage($"'{target.username}' '{byte1}' '{int1}' '{float1}' '{string1}' '{string2}'");
         }
 
-        public void OnCommandInvoke(ShPlayer player, int i = 1)
+        public void Command2(ShPlayer player, int i = 1)
         {
             player.svPlayer.SendGameMessage("int : " + i);
         }
 
-        public void OnCommandInvoke(ShPlayer player, string s = "testString")
+        public void Command3(ShPlayer player, string s = "testString")
         {
             player.svPlayer.SendGameMessage("string : " + s);
         }
 
-        public void OnCommandInvoke(ShPlayer player, ShPlayer p)
+        public void Command4(ShPlayer player, ShPlayer p)
         {
             player.svPlayer.SendGameMessage("player : " + p.username);
+        }
+
+        public void Command5(ShPlayer player)
+        {
+            player.svPlayer.SvOpenURL("https://discord.gg/wEB2ZGU", "Open Official BP Discord");
         }
     }
 }
