1fc0c41186c30c5df63db51e973347d0303f87d6
NongBenz <nongbenzgames@gmail.com>
2022-10-16 01:10:19 +0700

    Added TransportEvents with Abandoned and HazardDamage checks to GameSource

---
+++ GameSource/Types/Transport.cs
@@ -0,0 +1,65 @@
+﻿using BrokeProtocol.API;
+using BrokeProtocol.Entities;
+using BrokeProtocol.Utility;
+using UnityEngine;
+using System.Collections;
+using BrokeProtocol.Required;
+
+namespace BrokeProtocol.GameSource.Types
+{
+    public class Transport : TransportEvents
+    {
+        public override bool Spawn(ShEntity entity)
+        {
+            var t = entity as ShTransport;
+            entity.StartCoroutine(AbandonedCheck(t));
+            entity.StartCoroutine(CheckHazardDamage(t));
+            return true;
+        }
+
+        public IEnumerator AbandonedCheck(ShTransport transport)
+        {
+            var timeStarted = false;
+            var respawnTime = 0f;
+            var delay = new WaitForSeconds(3f);
+
+            while (true)
+            {
+                yield return delay;
+
+                if (!timeStarted)
+                {
+                    if (transport.IsAbandoned)
+                    {
+                        timeStarted = true;
+                        respawnTime = Time.time + 240f;
+                    }
+                }
+                else if (!transport.IsAbandoned)
+                {
+                    timeStarted = false;
+                }
+                else if (Time.time > respawnTime && (transport.svTransport.randomSpawn || transport.DistanceSqr(transport.svTransport.originalPosition) > Util.inviteDistanceSqr))
+                {
+                    timeStarted = false;
+                    transport.svTransport.Respawn();
+                }
+            }
+        }
+
+        public IEnumerator CheckHazardDamage(ShTransport transport)
+        {
+            // Don't damge during the spawn phase
+            var delay = new WaitForSeconds(2f);
+            yield return delay;
+            while (!transport.IsDead)
+            {
+                if (Time.time - transport.svTransport.collisionTime < 0.1f && transport.mainT.up.y < 0.3f || transport.isUnderwater && !(transport is ShBoat))
+                {
+                    transport.svTransport.Damage(DamageIndex.Null, transport.maxStat * 0.25f);
+                }
+                yield return delay;
+            }
+        }
+    }
+}
